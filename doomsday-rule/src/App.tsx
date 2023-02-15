8b21fd4251a69d9ed9be4df64c61ca51b8378bf0
hooliganny <samuelsyberswhite@gmail.com>
2023-02-15 21:45:16 +1300

    remove unneeded css, prettier and linter

--- doomsday-rule/src/App.tsx
+++ doomsday-rule/src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [guess, setGuess] = useState()
-  const [count, setCount] = useState(0)
-  const answer = 0 
+  const [guess, setGuess] = useState();
+  const [count, setCount] = useState(0);
+  const answer = 0;
   const handleChange = (event: any) => {
-    console.log(guess)
+    console.log(guess);
     setGuess(event.target.value);
   };
 
@@ -14,21 +14,21 @@ function App() {
     <div className="App">
       <h1>Doomsday Rule Practise</h1>
       <div className="card">
-        <p> 
-          ~Advice Advice~
-        </p>
-        <textarea value={guess} onChange={handleChange}/>
+        <p>~Advice Advice~</p>
+        <textarea value={guess} onChange={handleChange} />
       </div>
-      <button onClick={() => {
-        if (guess == answer) {
-          setCount((count) => count + 1)
-        }
-      }}>Guess!</button>
-      <p className="correct">
-        Number of guesses correct: {count}
-      </p>
+      <button
+        onClick={() => {
+          if (guess == answer) {
+            setCount((count) => count + 1);
+          }
+        }}
+      >
+        Guess!
+      </button>
+      <p className="correct">Number of guesses correct: {count}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
