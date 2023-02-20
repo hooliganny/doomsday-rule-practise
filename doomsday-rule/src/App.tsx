e50d3568975576ca9b600a739f7853b7beca65fe
hooliganny <samuelsyberswhite@gmail.com>
2023-02-20 21:25:58 +1300

    Working buttons,

--- doomsday-rule/src/App.tsx
+++ doomsday-rule/src/App.tsx
@@ -3,12 +3,19 @@ import "./App.css";
 
 function App() {
   function randomDate(start: Date, end: Date) {
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    return new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime())
+    );
   }
   function newDate() {
-    setAnswer(randomDate(new Date('1700-01-01T12:00:00'), new Date('2299-12-31T12:00:00')));
+    setAnswer(
+      randomDate(
+        new Date("1700-01-01T12:00:00"),
+        new Date("2299-12-31T12:00:00")
+      )
+    );
   }
-  enum Days {
+  const Days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -16,35 +23,65 @@ function App() {
     "Thursday",
     "Friday",
     "Saturay",
-  }
-  const [guess, setGuess] = useState('');
+  ];
+  const [guess, setGuess] = useState("");
   const [count, setCount] = useState(0);
   const [answer, setAnswer] = useState(new Date());
-  useEffect(()=> {
+  /*   useEffect(() => {
     newDate();
-  }, [count]);
-  console.log(answer.getDay());
+  }, [count]); */
+  console.log(answer, answer.getDay());
   const handleChange = (event: any) => {
     setGuess(event.target.value);
   };
-
+  const handleKeyDown = (event: any) => {
+    console.log(event.key);
+    if (event.key === "Enter") {
+      validateGuess();
+    }
+  };
+  const validateGuess = () => {
+    if (
+      (guess as unknown as number) == answer.getDay() ||
+      guess == Days[answer.getDay()]
+    ) {
+      setCount((count) => count + 1);
+      setGuess("");
+      newDate();
+    }
+  };
+  const revealHelp = () => {
+    // maybe toggle display? is there a modular way to do this?
+    alert("No implementation yet, sorry");
+  };
+  const renderButtons = Days.map((day) => {
+    return (
+      // Need to press twice for some reason, look into this
+      <button
+        key={day}
+        onClick={() => {
+          setGuess(day);
+          validateGuess();
+        }}
+      >
+        {day}
+      </button>
+    );
+  });
   return (
     <div className="App">
       <h1>Doomsday Rule Practise</h1>
       <div className="card">
         <p>~Advice Advice~</p>
-        <textarea value={guess} onChange={handleChange} />
+        <div className="button-wrapper">{renderButtons}</div>
+        <textarea
+          value={guess}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
       </div>
-      <button
-        onClick={() => {
-          if (((guess as unknown as number) == answer.getDay()) || (guess == Days[answer.getDay()])) {
-            setCount((count) => count + 1);
-            setGuess('');
-          }
-        }}
-      >
-        Guess!
-      </button>
+      <button onClick={validateGuess}>Guess!</button>
+      <button onClick={revealHelp}>Help!</button>
       <p className="correct">Number of guesses correct: {count}</p>
     </div>
   );
