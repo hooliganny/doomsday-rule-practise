c4b23a38e857a054f4ee70186f62c539140da3fa
hooliganny <samuelsyberswhite@gmail.com>
2023-09-17 15:41:49 +1200

    Use enter to answer, better funcs, style changes

--- doomsday-rule/src/App.tsx
+++ doomsday-rule/src/App.tsx
@@ -1,86 +1,87 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 
+const Days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const MAX_NUMBER_OF_GUESSES = 3;
+
+const EARLIEST_DAY = new Date("1700-01-01T12:00:00");
+const LATEST_DAY = new Date("2299-12-31T12:00:00");
+
+function randomDate(start: Date, end: Date) {
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
+}
+
 function App() {
-  function randomDate(start: Date, end: Date) {
-    return new Date(
-      start.getTime() + Math.random() * (end.getTime() - start.getTime())
-    );
-  }
-  function newDate() {
-    setAnswer(
-      randomDate(
-        new Date("1700-01-01T12:00:00"),
-        new Date("2299-12-31T12:00:00")
-      )
-    );
-  }
-  const Days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  // const refs = {
-  //   guessRef: useRef(""),
-  // };
   const [guess, setGuess] = useState("");
   const [rightCount, setRightCount] = useState(0);
-  // const [wrongCount, setWrongCount] = useState(0); Do I want to count this?? - How does it affect the validation in the useEffect?
+  const [wrongCount, setWrongCount] = useState(MAX_NUMBER_OF_GUESSES);
   const [answer, setAnswer] = useState(new Date());
-  function handleChange(event: any) {
-    setGuess(event.target.value);
+  const keyboardInputRef = useRef<HTMLInputElement | null>(null);
+
+  function newDate() {
+    setAnswer(randomDate(EARLIEST_DAY, LATEST_DAY));
   }
-  // No longer necessary since validateGuess() is gone
-  /* function handleKeyDown(event: any) {
+
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
     if (event.key === "Enter") {
-      // validateGuess();
+      setGuess(keyboardInputRef.current?.value || "");
     }
-  } */
-  function buttonPress(day: string) {
-    setGuess(day);
-    // validateGuess();
   }
-  /* function validateGuess() {
-    console.log(guess);
-    // Guess is not updating fast enough, hence the need to double press, has something to do with async nature of react i think, research this
-    if (
-      (guess as unknown as number) == answer.getDay() ||
-      guess == Days[answer.getDay()]
-    ) {
-      setRightCount((rightCount) => rightCount + 1);
-      setGuess("");
-      newDate();
-    }
-  } */
+
+  const buttonPress = useCallback((day: string) => {
+    return () => setGuess(day);
+  }, []);
 
   useEffect(() => {
-    // Might be inefficient to be updating every time something changes, but it fixes the problem from validateGuess()
     if (
-      (guess as unknown as number) == answer.getDay() ||
-      guess == Days[answer.getDay()]
+      guess !== "" &&
+      (Number(guess) === answer.getDay() || guess == Days[answer.getDay()])
     ) {
       setRightCount((rightCount) => rightCount + 1);
       setGuess("");
       newDate();
+      if (
+        keyboardInputRef.current !== undefined &&
+        keyboardInputRef.current !== null
+      ) {
+        keyboardInputRef.current.value = ""; // Clear the input box
+      }
     }
   }, [guess]);
 
   function revealHelp() {
-    // maybe toggle display? is there a modular way to do this?
     alert("No implementation yet, sorry");
   }
-  const renderButtons = Days.map((day) => {
-    return (
-      // Need to press twice for some reason, look into this
-      <button key={day} onClick={() => buttonPress(day)}>
-        {day}
-      </button>
-    );
-  });
+
+  const DayButtons = useMemo(
+    () => (
+      <div className="button-wrapper">
+        {Days.map((day) => {
+          return (
+            <button
+              className="day-button"
+              key={day}
+              onClick={buttonPress(day)} // Pass the day directly
+            >
+              {day}
+            </button>
+          );
+        })}
+      </div>
+    ),
+    []
+  );
 
   return (
     <div className="App">
@@ -90,30 +91,28 @@ function App() {
           {answer.toLocaleDateString()} <br />
         </h2>
         <p>
-          <i>
-            Month/Day/Year
-            {/* {Intl.DateTimeFormat(answer).prototype.resolvedOptions().dateStyle}  Was trying to make it so that it would show the timezone of the system they are using */}
-          </i>
+          <i>Month/Day/Year</i>
         </p>
         <h2>
-          This will be removed later, answer: {answer.getDay()} or{" "}
+          This will be removed later, answer: {answer.getDay()}/
           {Days[answer.getDay()]}
         </h2>
 
-        <p>Find the day of the week of this date!</p>
-        <div className="button-wrapper">{renderButtons}</div>
-        <input
-          type="text"
-          value={guess}
-          onChange={handleChange}
-          // onKeyDown={handleKeyDown}
-          // ref={refs.guessRef}
-        />
-      </div>
-      <button>Guess!</button>
-      <button onClick={revealHelp}>Tutorial</button>
+        <h2>Find the day of the week of this date!</h2>
 
-      <p className="correct">Number of guesses correct: {rightCount}</p>
+        {DayButtons}
+
+        <div className="answer-section">
+          <input type="text" ref={keyboardInputRef} onKeyDown={handleKeyDown} />
+          <button>Guess!</button>
+        </div>
+
+        <p className="correct">Number of guesses correct: {rightCount}</p>
+      </div>
+      <div className="extra-buttons">
+        <button onClick={revealHelp}>Step-by-Step Tutorial</button>
+        <button onClick={revealHelp}>What is this?</button>
+      </div>
     </div>
   );
 }
